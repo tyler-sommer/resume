5a6cf29ab717ff96ae1dbdcca72b3caf6ab2b29a
Tyler Sommer <sommertm@gmail.com>
2013-08-07 22:31:12 -0700

    Moved things into some folders, mainly to add a specific webroot below everything else

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@
 
 $ch = curl_init('https://api.github.com/markdown/raw');
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents('resume.md'));
+curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents(__DIR__ . '/../resume.md'));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: text/plain'));
@@ -11,6 +11,6 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: text/plain'));
 $response = curl_exec($ch);
 
 
-$template = file_get_contents('template.html');
+$template = file_get_contents(__DIR__ . '/../views/template.html');
 
-file_put_contents('index.html', str_replace('{{ body }}', $response, $template));
+file_put_contents(__DIR__ . '/../web/index.html', str_replace('{{ body }}', $response, $template));
